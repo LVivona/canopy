852cb17907cf2b96ab37bb492b376f555a1347ae
Luca Vivona <lucavivona01@gmail.com>
2025-03-12 02:39:31 -0400

    chore(docs): update docs for new interface

--- docs/trait.impl/core/default/trait.Default.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["canopy",[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"enum\" href=\"canopy/enum.Node.html\" title=\"enum canopy::Node\">Node</a>&lt;T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a>,</div>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[458]}
--- docs/trait.impl/core/fmt/trait.Debug.js
+++ docs/trait.impl/core/fmt/trait.Debug.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["canopy",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"canopy/error/enum.NodeError.html\" title=\"enum canopy::error::NodeError\">NodeError</a>"],["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"canopy/enum.Node.html\" title=\"enum canopy::Node\">Node</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["canopy",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"canopy/error/enum.NodeError.html\" title=\"enum canopy::error::NodeError\">NodeError</a>"],["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"canopy/enum.Node.html\" title=\"enum canopy::Node\">Node</a>&lt;T&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a>,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[647]}
+//{"start":57,"fragment_lengths":[687]}
